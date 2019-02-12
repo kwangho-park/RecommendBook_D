1cc3a1b48cb1790bee9aee0a062b8d642e592c94
kwangho-park (solt) <44250982+kwangho-park@users.noreply.github.com>
2019-02-12 16:36:21 +0900

    Add files via upload

--- src/clientInfo/MemberDB.java
+++ src/clientInfo/MemberDB.java
@@ -1,32 +1,22 @@
 package clientInfo;
 
+// = 임시 DAO + DB의 역활의 클래스
 public class MemberDB {
-
-	// data type (임시)
+	
 	// id, pw, name, email, address = String
 	
 	// element 5개 단위로 동일한 회원의 정보
 	private String[] memberArr = new String[20];		// 회원은 4명만 가입받음 (임시)
 	private int startNum = 0;
+
 	
-	// [test]
-	public void memberArrTest() {
-		this.memberArr[0] = "admin";
-		this.memberArr[1] = "1234";
-		
-		this.memberArr[5] = "a";
-		this.memberArr[10] = "b";
-		this.memberArr[15] = "c";
-	}
-
-
-	// 회원정보 셋팅용 get, set
 	public String[] getMemberArr(){
-		return this.memberArr;		
+		return this.memberArr;
 	}
-
+	
+	// 회원정보 setter
 	public void setMember(String id, String pw, String name, String email, String address){	
-		
+
 		this.memberArr[startNum] = id;	
 		this.memberArr[startNum+1] = pw;
 		this.memberArr[startNum+2] = name;
@@ -34,29 +24,10 @@ public class MemberDB {
 		this.memberArr[startNum+4] = address;
 		
 		startNum += 5;
-	}
+	} // setMember() END
 	
 	
-	
-	// test
-	public void deleteMemberArr() {
-		for(int index=0 ; index<20 ; index++) {
-			this.memberArr[index] = null;
-		}
-	}
-	
-	
-	
-	
-	
-	public String getPw(){	return this.memberArr[1];	}
-	public void setPw(String pw){	this.memberArr[1] = pw;	}
-
-	public String getName(){	return this.memberArr[2];	}
-	public void setName(String name){	this.memberArr[2] = name;	}
-	
-	
-	// 배열이 full 여부 확인
+	// 배열 element의 full check //
 	public boolean isFull() {
 		
 		boolean result = false;
@@ -66,10 +37,10 @@ public class MemberDB {
 		}
 		
 		return result;
-	}
+	} // isFull() END
 
-	 
-	// id중복 확인용 method
+
+	// id중복 확인용 method //
 	// 중복되는 id가 배열에 존재 할 경우 false 반환
 	public boolean idFilter(String id) {
 
@@ -82,8 +53,29 @@ public class MemberDB {
 			}
 		}
 		return result;
-		
+
 	} // idFilter() END
+	
+	
+	
+	
+	// 로그인 필터링을 위한 id,pw 비교 메소드 // 
+	public boolean loginFilter(String id, String pw) {
+
+		boolean result = false;
+
+		for(int index=0 ; index<4 ; index++) {
+
+			if(id.equals(this.memberArr[index*5])) {		// id 저장하는 element index : 0,5,10,15
+				
+				if(pw.equals(this.memberArr[index*5+1])) {
+					result = true;	
+				}	
+			}
+		}
+		return result;
+
+	} // loginFilter() END
 	
 	
 } // MemberDB END
